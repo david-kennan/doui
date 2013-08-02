ae87fc1f7b522f030637d03ebce663c5b3a7a2e3
Sergey Gadhzilov <sgaddreams@gmail.com>
2013-08-02 22:36:24 +0300

    Added progress bar when screen is changed.

--- doui_android/src/co/usersource/doui/sync/SyncFinishHandler.java
+++ doui_android/src/co/usersource/doui/sync/SyncFinishHandler.java
@@ -1,6 +1,7 @@
 package co.usersource.doui.sync;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,21 +12,36 @@ public class SyncFinishHandler extends BroadcastReceiver {
 	
 	private Intent finishActivity;
 	private Activity parentActivity;
+	private ProgressDialog progressDlg;
 	
 	public SyncFinishHandler(Activity parent) {
 		IntentFilter syncIntentFilter = new IntentFilter(SyncAdapter.ACTION_SYNC_FINISHED);
 		parentActivity = parent;
 		parentActivity.registerReceiver(this, syncIntentFilter);
+		this.ShowProgressBar(true);
 	}
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		parentActivity.startActivity(finishActivity);
 		parentActivity.unregisterReceiver(this);
+		this.ShowProgressBar(false);
 	}
 	
 	public void  setFinishActivity(Intent activity) {
 		finishActivity = activity;
+	}
+	
+	
+	public void ShowProgressBar(Boolean isShow)
+	{
+		if(isShow)
+		{
+			progressDlg = ProgressDialog.show(parentActivity, "Please wait", "Loading...");
+		}
+		else{
+			progressDlg.dismiss();
+		}
 	}
 
 }
